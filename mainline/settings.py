6327e61d3e668074330ea441935276725400a12f
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-07-21 10:23:09 +0900

    Change debug to false for release

--- mainline/settings.py
+++ mainline/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k-kaj7&h4&t8l&-2i_@t!5llu4-wc^&-=qo6i-+&n=(^g913k$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 

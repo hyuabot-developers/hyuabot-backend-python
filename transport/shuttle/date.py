1064683f409bbcbe93eeffa15d9efb58d8da1574
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-09-25 15:53:18 +0900

    complete func to get shuttle departure

--- transport/shuttle/date.py
+++ transport/shuttle/date.py
@@ -1,36 +1,50 @@
 import requests, requests_cache
+from workalendar.asia import SouthKorea
+
 from datetime import datetime
 import calendar
 
-def is_semester(modified=False):
-    now = datetime.now()
+cal = SouthKorea()
+now = datetime.now()
+
+def is_semester(modified=False, date_to_know=now):
 
     # 학기중, 계절학기, 방학 중인지 구별 코드
     requests_cache.install_cache('timetable_cache')
     req = requests.get(url="https://raw.githubusercontent.com/jil8885/hanyang-shuttle-timetable/main/date.json")
     result = req.json()
-    for stop_date in result['halt']:
-        halt_date = datetime.strptime(term['start'], "%m-%d")
-        if (now.month, now.day) == (halt_date.month, halt_date.day):
-            return 'halt'
-    for key in list(result.keys()).remove('halt'):
+
+    for key in [x for x in list(result.keys()) if x not in ['holiday', 'halt']]:
         for term in result[key]:
             startTime = datetime.strptime(term['start'], "%m-%d")
             endTime = datetime.strptime(term['end'], "%m-%d")
             if endTime <= startTime:
-                startTime = startTime.replace(year=now.year)
-                endTime = endTime.replace(year=now.year)
+                startTime = startTime.replace(year=date_to_know.year)
+                endTime = endTime.replace(year=date_to_know.year)
             else:
-                startTime = startTime.replace(year=now.year)
-                endTime = endTime.replace(year=now.year + 1)
-
-            if startTime.day > calendar.monthrange(startTime.year, startTime.month)[1]:
-                startTime = startTime.replace(day=calendar.monthrange(startTime.year, startTime.month)[1])
-
-            if endTime.day > calendar.monthrange(endTime.year, endTime.month)[1]:
-                endTime = endTime.replace(day=calendar.monthrange(endTime.year, endTime.month)[1])
-
-            endTime = endTime.replace(hour=23, minute=59, second=59)
+                startTime = startTime.replace(year=date_to_know.year)
+                endTime = endTime.replace(year=date_to_know.year + 1)
             
-            if now >= startTime and now <= endTime:
-                return key
+            if date_to_know >= startTime and date_to_know < endTime:
+                term = key
+
+
+    # 운행 중지 일자
+    for stop_date in result['halt']:
+        halt_date = datetime.strptime(stop_date, "%m-%d")
+        if (date_to_know.month, date_to_know.day) == (halt_date.month, halt_date.day):
+            term = 'halt'
+
+    # 평일/주말 구분
+    if date_to_know.weekday() in [5, 6] or not cal.is_working_day(date_to_know):
+        day = 'weekend'
+    else:
+        day = 'week'
+
+    # 공휴일 구분
+    for holiday_date in result['holiday']:
+        holiday_date = datetime.strptime(holiday_date, "%m-%d")
+        if (date_to_know.month, date_to_know.day) == (holiday_date.month, holiday_date.day):
+            day = 'weekend'
+
+    return term, day
--- transport/shuttle/get_info.py
+++ transport/shuttle/get_info.py
@@ -1,18 +1,78 @@
 # External Module
-import json
+import json, os, platform
 from datetime import datetime
-from workalendar.asia import SouthKorea
 
 # Internal Module
-from transport.shuttle.date import is_semester
+from transport.shuttle.date import is_semester # To get which file to use
 
-cal = SouthKorea()
 now = datetime.now()
-def get_departure_info():
-    bool_semester = is_semester()
+
+def get_departure_info(dest_stop):
+    # 학기 여부, 주말 여부 연산
+    bool_semester, bool_weekend = is_semester()
+    
+    # 학기 여부에 따라 json 경로 수정
     term = {
         'halt':'/halt',
         'semester':'/semester',
         'vacation_session':'/vacation_session',
-        
+        'vacation':'/vacation'
     }
+
+    # 발화 내용에 따라 json 경로 수정
+    stop = {
+        '기숙사':'Residence',
+        '셔틀콕':'ShuttleCock_O',
+        '한대앞역':'Subway',
+        '예술인A':'Yesulin',
+        '셔틀콕 건너편':'ShuttleCock_I',
+    }
+
+
+    # 운행 중지 일자라면 중지한다고 반환
+    if term[bool_semester] == '/halt':
+        return '오늘 셔틀 운행을 하지 않습니다.'
+    else:
+        # json 파일 로드
+        current_dir = os.path.dirname(os.path.realpath(__name__))
+        dest_timetable = f'{current_dir}/transport/shuttle/timetable{term[bool_semester]}/{bool_weekend}/{stop[dest_stop]}_{bool_weekend}.json'
+        
+        # Windows 라면, 경로명 수정
+        if platform.system() == 'Windows':
+            dest_timetable = dest_timetable.replace('/', '\\')
+   
+        with open(dest_timetable, 'r') as raw_json:
+            timetable = json.load(raw_json)
+
+        # 표출할 경로
+        bus_to_come_c = []
+        bus_to_come_dh = []
+        bus_to_come_dy = []
+
+        key = list(timetable.keys())[0]
+        for depart_info in timetable[key]:
+            # 항목별 시간
+            depart_time = datetime.strptime(depart_info['time'], '%H:%M')
+            depart_time = depart_time.replace(year=now.year, month=now.month, day=now.day)
+            print(depart_time)
+            if depart_time >= now:       
+                # 순환버스 도착 정보 최대 2개
+                if (depart_info['type'] == 'C' or dest_stop == '예술인A') and len(bus_to_come_c) < 2:
+                    bus_to_come_c.append(depart_time)
+                # 한대앞 직행 버스 도착 정보 최대 2개
+                elif (depart_info['type'] == 'DH' or (dest_stop == '한대앞역' and not depart_info['type'])) and len(bus_to_come_dh) < 2:
+                    bus_to_come_dh.append(depart_time)
+                elif depart_info['type'] == 'DY' and len(bus_to_come_dh) < 2:
+                    bus_to_come_dy.append(depart_time)
+                elif (dest_stop == '셔틀콕' or dest_stop == '기숙사') and len(bus_to_come_dh) >= 2 and len(bus_to_come_dy) >= 2 and len(bus_to_come_c) >= 2:
+                    break
+                elif dest_stop == '한대앞역' and len(bus_to_come_dh) >= 2 and len(bus_to_come_c) >= 2:
+                    break
+                elif dest_stop == '예술인A' and len(bus_to_come_dy) >= 2 and len(bus_to_come_c) >= 2:
+                    break
+        return bus_to_come_dh, bus_to_come_dy, bus_to_come_c
+                
+
+
+
+    

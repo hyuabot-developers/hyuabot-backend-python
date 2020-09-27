0852630573f50f6458398489da46f268393ffc34
Jeongin Lee <jil8885@hanyang.ac.kr>
2020-09-27 16:16:33 +0900

    Update food func

--- firebase/firebase_init.py
+++ firebase/firebase_init.py
@@ -2,6 +2,6 @@ import firebase_admin
 
 
 def get_cred():
-    # cred = firebase_admin.credentials.ApplicationDefault()
-    cred = firebase_admin.credentials.Certificate('Z:\\Firebase auth\\personal-sideprojects-firebase-adminsdk.json')
+    cred = firebase_admin.credentials.ApplicationDefault()
+    # cred = firebase_admin.credentials.Certificate('Z:\\Firebase auth\\personal-sideprojects-firebase-adminsdk.json')
     return cred

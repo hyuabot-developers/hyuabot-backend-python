bb12905a112c78d68b089df4131bb34f85d05393
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-12-14 20:53:57 +0900

    feat: Create tables for building / room

---
+++ src/model/building.py
@@ -0,0 +1,37 @@
+from sqlalchemy import ForeignKeyConstraint, Integer, Float, Text, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from model import Base
+
+
+class Building(Base):
+    __tablename__ = "building"
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ["campus_id"],
+            ["campus.campus_id"],
+        ),
+    )
+
+    id: Mapped[str] = mapped_column("id", String(15))
+    name: Mapped[str] = mapped_column("name", String(30))
+    campus_id: Mapped[int] = mapped_column("campus_id", Integer)
+    latitude: Mapped[float] = mapped_column("latitude", Float)
+    longitude: Mapped[float] = mapped_column("longitude", Float)
+    url: Mapped[str] = mapped_column("url", Text)
+
+
+class Room(Base):
+    __tablename__ = "room"
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ["building_id"],
+            ["building.id"],
+        ),
+    )
+
+    id: Mapped[int] = mapped_column("id", Integer)
+    building_id: Mapped[str] = mapped_column("building_id", String(15))
+    name: Mapped[str] = mapped_column("name", String(30))
+    floor: Mapped[str] = mapped_column("floor", String(10))
+    number: Mapped[str] = mapped_column("number", String(10))

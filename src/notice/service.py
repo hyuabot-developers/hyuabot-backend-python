831919eb273ef367edf6106ff3736f21fec410ac
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-01-29 18:00:52 -0700

    fix: Notice router to apply query method

--- src/notice/service.py
+++ src/notice/service.py
@@ -9,19 +9,19 @@ from notice.schemas import (
 )
 
 
-async def list_notice_category() -> list[dict[str, str]]:
+async def list_notice_category() -> list[NoticeCategory]:
     select_query = select(NoticeCategory)
     return await fetch_all(select_query)
 
 
-async def list_notice_category_filter(name: str) -> list[dict[str, str]]:
+async def list_notice_category_filter(name: str) -> list[NoticeCategory]:
     select_query = select(NoticeCategory).filter(NoticeCategory.name.like(f"%{name}%"))
     return await fetch_all(select_query)
 
 
 async def create_notice_category(
     new_notice_category: CreateNoticeCategoryRequest,
-) -> dict[str, str] | None:
+) -> NoticeCategory | None:
     insert_query = (
         insert(NoticeCategory)
         .values(
@@ -34,12 +34,12 @@ async def create_notice_category(
     return await fetch_one(insert_query)
 
 
-async def get_notice_category(notice_category_id: int) -> dict[str, str] | None:
+async def get_notice_category(notice_category_id: int) -> NoticeCategory | None:
     select_query = select(NoticeCategory).where(NoticeCategory.id == notice_category_id)
     return await fetch_one(select_query)
 
 
-async def get_notice_category_by_name(name: str) -> dict[str, str] | None:
+async def get_notice_category_by_name(name: str) -> NoticeCategory | None:
     select_query = select(NoticeCategory).where(NoticeCategory.name == name)
     return await fetch_one(select_query)
 
@@ -49,7 +49,7 @@ async def delete_notice_category(notice_category_id: int) -> None:
     await execute_query(delete_query)
 
 
-async def get_notice_list(notice_category_id: int) -> list[dict[str, str]]:
+async def get_notice_list(notice_category_id: int) -> list[Notice]:
     select_query = select(Notice).where(Notice.category_id == notice_category_id)
     return await fetch_all(select_query)
 
@@ -57,7 +57,7 @@ async def get_notice_list(notice_category_id: int) -> list[dict[str, str]]:
 async def get_notice(
     category_id: int,
     notice_id: int,
-) -> dict[str, str] | None:
+) -> Notice | None:
     select_query = select(Notice).where(
         Notice.category_id == category_id,
         Notice.id == notice_id,
@@ -65,7 +65,7 @@ async def get_notice(
     return await fetch_one(select_query)
 
 
-async def get_notice_by_id(notice_id: int) -> dict[str, str] | None:
+async def get_notice_by_id(notice_id: int) -> Notice | None:
     select_query = select(Notice).where(Notice.id == notice_id)
     return await fetch_one(select_query)
 
@@ -74,7 +74,7 @@ async def create_notice(
     user_id: str,
     category_id: int,
     new_notice: CreateNoticeReqeust,
-) -> dict[str, str] | None:
+) -> Notice | None:
     insert_query = (
         insert(Notice)
         .values(
@@ -107,7 +107,7 @@ async def update_notice(
     notice_category_id: int,
     notice_id: int,
     new_notice: UpdateNoticeRequest,
-) -> dict[str, str] | None:
+) -> Notice | None:
     update_query = (
         update(Notice)
         .where(

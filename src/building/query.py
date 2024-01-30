906db4d4c94047e6821a0af60c92edd94b12665d
Jeongin Lee <jil8885@hanyang.ac.kr>
2024-01-30 10:19:19 -0700

    feat: Implement building query via GraphQL

--- src/building/query.py
+++ src/building/query.py
@@ -1,16 +1,23 @@
-from typing import Optional
+from typing import Optional, Callable
 
 import strawberry
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload, load_only
 
 from database import fetch_all
-from model.building import Building
+from model.building import Building, Room
 
 
 @strawberry.type
 class RoomQuery:
     name: str = strawberry.field(description="Room name")
     number: str = strawberry.field(description="Room number")
+    building_name: str = strawberry.field(
+        description="Building name",
+        name="buildingName",
+    )
+    latitude: float = strawberry.field(description="Building latitude")
+    longitude: float = strawberry.field(description="Building longitude")
 
 
 @strawberry.type
@@ -20,7 +27,6 @@ class BuildingQuery:
     latitude: float = strawberry.field(description="Building latitude")
     longitude: float = strawberry.field(description="Building longitude")
     url: Optional[str] = strawberry.field(description="Blog URL")
-    rooms: list[RoomQuery] = strawberry.field(description="Rooms in the building")
 
 
 async def resolve_building(
@@ -42,25 +48,44 @@ async def resolve_building(
     if name is not None:
         conditions.append(Building.name.like(f"%{name}%"))
     select_query = select(Building).where(*conditions)
-    building_list = await fetch_all(select_query)
-    return list(
-        map(
-            lambda building: BuildingQuery(
-                _id=building["id"],
-                name=building["name"],
-                latitude=building["latitude"],
-                longitude=building["longitude"],
-                url=building["url"],
-                rooms=list(
-                    map(
-                        lambda room: RoomQuery(
-                            name=room["name"],
-                            number=room["number"],
-                        ),
-                        building["rooms"],
-                    ),
-                ),
-            ),
-            building_list,
-        ),
+    building_list: list[Building] = await fetch_all(select_query)
+    mapping_func: Callable[[Building], BuildingQuery] = lambda building: BuildingQuery(
+        _id=building.id_,
+        name=building.name,
+        latitude=building.latitude,
+        longitude=building.longitude,
+        url=building.url,
     )
+    return list(map(mapping_func, building_list))
+
+
+async def resolve_room(
+    building_name: Optional[str] = None,
+    name: Optional[str] = None,
+    number: Optional[str] = None,
+) -> list[RoomQuery]:
+    conditions = []
+    if building_name is not None:
+        conditions.append(Room.building_name.like(f"%{building_name}%"))
+    if name is not None:
+        conditions.append(Room.name.like(f"%{name}%"))
+    if number is not None:
+        conditions.append(Room.number.like(f"%{number}%"))
+    select_query = (
+        select(Room)
+        .where(*conditions)
+        .options(
+            joinedload(Room.building).options(
+                load_only(Building.name, Building.latitude, Building.longitude),
+            ),
+        )
+    )
+    room_list: list[Room] = await fetch_all(select_query)
+    mapping_func: Callable[[Room], RoomQuery] = lambda room: RoomQuery(
+        name=room.name,
+        number=room.number,
+        latitude=room.building.latitude,
+        longitude=room.building.longitude,
+        building_name=room.building.name,
+    )
+    return list(map(mapping_func, room_list))
--- src/query/query.py
+++ src/query/query.py
@@ -1,6 +1,6 @@
 import strawberry
 
-from building.query import BuildingQuery, resolve_building
+from building.query import BuildingQuery, resolve_building, RoomQuery, resolve_room
 
 
 @strawberry.type
@@ -12,4 +12,8 @@ class Query:
     building: list[BuildingQuery] = strawberry.field(
         resolver=resolve_building,
         description="Building query",
+    )
+    room: list[RoomQuery] = strawberry.field(
+        resolver=resolve_room,
+        description="Room query",
     )

58052a10584891a9152aef197d301f411eaffa4f
Jeongin Lee <jil8885@gmail.com>
2025-03-03 17:13:36 +0900

    refactor: Remove duplicated notice schema

--- src/notice/schemas.py
+++ src/notice/schemas.py
@@ -64,34 +64,13 @@ class UpdateNoticeRequest(BaseModel):
         }
 
 
-class NoticeCategoryListItemResponse(BaseModel):
-    id_: Annotated[int, Field(alias="id", ge=1)]
-    name: Annotated[str, Field(max_length=20, alias="name")]
-
-
-class NoticeCategoryListResponse(BaseModel):
-    data: Annotated[list[NoticeCategoryListItemResponse], Field(alias="data")]
-
-
 class NoticeCategoryDetailResponse(BaseModel):
     id_: Annotated[int, Field(alias="id", ge=1)]
     name: Annotated[str, Field(max_length=20, alias="name")]
 
 
-class NoticeListItemResponse(BaseModel):
-    id_: Annotated[int, Field(alias="id", ge=1)]
-    title: Annotated[str, Field(max_length=100, alias="title")]
-    url: Annotated[str, Field(max_length=100, alias="url")]
-    user_id: Annotated[str, Field(alias="userID")]
-    language: Annotated[str, Field(max_length=10, alias="language")]
-    expired_at: Annotated[
-        Optional[datetime.datetime],
-        Field(alias="expiredAt"),
-    ]
-
-
-class NoticeListResponse(BaseModel):
-    data: Annotated[list[NoticeListItemResponse], Field(alias="data")]
+class NoticeCategoryListResponse(BaseModel):
+    data: Annotated[list[NoticeCategoryDetailResponse], Field(alias="data")]
 
 
 class NoticeDetailResponse(BaseModel):
@@ -104,3 +83,7 @@ class NoticeDetailResponse(BaseModel):
         Optional[datetime.datetime],
         Field(alias="expiredAt"),
     ]
+
+
+class NoticeListResponse(BaseModel):
+    data: Annotated[list[NoticeDetailResponse], Field(alias="data")]
